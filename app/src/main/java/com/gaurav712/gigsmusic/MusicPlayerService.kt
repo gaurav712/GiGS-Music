64093ad29d9573a1783d983c8f521b32fd3c5e2d
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2020-11-10 13:49:42 +0530

    added basic layout for playlist view

--- app/src/main/java/com/gaurav712/gigsmusic/MusicPlayerService.kt
+++ app/src/main/java/com/gaurav712/gigsmusic/MusicPlayerService.kt
@@ -4,6 +4,8 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioAttributes.CONTENT_TYPE_MUSIC
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.IBinder
@@ -72,6 +74,11 @@ class MusicPlayerService: Service() {
 
     fun loadMedia(context: Context, mediaUri: Uri) {
         mediaPlayer.setDataSource(context, mediaUri)
+        mediaPlayer.setAudioAttributes(
+            AudioAttributes.Builder().
+            setContentType(CONTENT_TYPE_MUSIC).
+            build()
+        )   // to set the stream to music
         mediaPlayer.prepare()
     }
 
